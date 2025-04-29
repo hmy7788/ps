1c40d92bde2b054ee525304c539309cd1cb1bff4
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-04-29 17:47:04 +0900

    [level 1] Title: 숫자 짝꿍, Time: 22.66 ms, Memory: 29.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+string solution(string X, string Y) {
+    vector<int> countX(10, 0);
+    vector<int> countY(10, 0);
+    string answer;
+    
+    for(char c : X) {
+        countX[int(c) - '0'] += 1;
+    }
+    for(char c : Y) {
+        countY[int(c) - '0'] += 1;
+    }
+    
+    for(int i=9; i>=0; i--) {
+        int count = min(countX[i], countY[i]);
+        answer += string(count, i + '0');
+    }
+    
+    if(answer.size() == 0) return "-1";
+    if(answer[0] == '0') return "0";
+    else return answer;
+    
+    
+    
+}
