96118f2ed93a98b8eda0cdfe0b9a4143d3d61051
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-10-17 16:19:27 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,13 @@
+def h_m_to_m(h, m):
+    return h * 60 + m
+
+def m_to_h_m(m):
+    if(m // 60 >= 24):
+        return (m//60)%24, m % 60
+    return m // 60, m % 60
+
+h, m = map(int, input().split())
+c = int(input())
+m = h_m_to_m(h, m)
+h, m = m_to_h_m(m+c)
+print(h, m)
