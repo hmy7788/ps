2822865c82c2bcddd1a9520545c54f05ef779eb2
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-10-20 20:33:55 +0900

    Refactor interval sum calculation for clarity

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,18 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def solution(n,m):
-    l = list(map(int,input().split()))
-    prefix_sum_list = [0]
-    sum = 0
+N, M = map(int, input().split())
+l = list(map(int, input().split()))
+p = [0 for _ in range(len(l)+1)]
 
-    for i in l:
-        sum += i
-        prefix_sum_list.append(sum)
+for i in range(1, len(p)):
+    p[i] = l[i-1] + p[i-1]
 
-    for i in range(m):
-        left,right = map(int,input().split())
-        print(prefix_sum_list[right] - prefix_sum_list[left-1])
-
-n,m = map(int, input().split())
-solution(n,m)
+for _ in range(M):
+    i, j = map(int, input().split())
+    print(p[j] - p[i-1])
