d3b97a85f1a991425a174d2d62cb9f5771b375b4
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-10-24 02:10:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 500 ms, Memory: 73496 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(G, R, visited):
+    global count
+    visited[R] = count
+    count += 1
+    for nv in G[R]:
+        if(visited[nv] == 0):
+            dfs(G, nv, visited)
+
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+G = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+count = 1
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    G[i].append(j)
+    G[j].append(i)
+
+for i in range(1, N+1):
+    G[i].sort()
+
+dfs(G, R, visited)
+print('\n'.join(map(str, visited[1:])))
