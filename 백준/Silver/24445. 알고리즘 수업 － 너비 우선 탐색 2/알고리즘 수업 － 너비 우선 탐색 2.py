7b8cda4fbff9033e0f11c95f836b0e4f6246f923
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-10-24 02:29:57 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 500 ms, Memory: 67456 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque 
+
+def bfs(G, R, visited):
+    count = 1
+    visited[R] = count
+    q = deque()
+    for nv in G[R]:
+        q.append(nv)
+    while(q):
+        u = q.popleft()
+        if(visited[u] == 0):
+            count += 1
+            visited[u] = count
+            for nv in G[u]:
+                q.append(nv)
+    return visited
+
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+G = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+count = 1
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    G[i].append(j)
+    G[j].append(i)
+
+for i in range(1, N+1):
+    G[i].sort(reverse=True)
+
+visited = bfs(G, R, visited)
+print('\n'.join(map(str, visited[1:])))
