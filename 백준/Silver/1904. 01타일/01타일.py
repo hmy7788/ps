bf177cbbad7d2af25db550d0ecad6fe74077c64b
허민엽 <155165269+hmy7788@users.noreply.github.com>
2025-10-29 00:41:52 +0900

    [Silver III] Title: 01타일, Time: 212 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,10 @@
+def tile_dp(N):
+    if(N == 1): return 1
+    dp = [0] * (N+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+    return dp[N]
+
+print(tile_dp(int(input())))
